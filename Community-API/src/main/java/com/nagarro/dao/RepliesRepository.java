901deef34a1cc1ed5c91d7430e9a2c6873b296fd
GautamMalik <68327414+GautamMalik@users.noreply.github.com>
2024-10-10 13:42:59 +0530

    Delete Community-API directory

--- Community-API/src/main/java/com/nagarro/dao/RepliesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nagarro.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nagarro.entity.RepliesEntity;
-
-public interface RepliesRepository extends JpaRepository<RepliesEntity, Integer>{
-
-}
--- Community-API/src/main/java/com/nagarro/service/ReplyService.java
+++
@@ -1,9 +0,0 @@
-package com.nagarro.service;
-
-import com.nagarro.entity.QuestionEntity;
-import com.nagarro.entity.RepliesEntity;
-
-public interface ReplyService {
-	public QuestionEntity AddReply(int id , RepliesEntity e1);
-	
-}
